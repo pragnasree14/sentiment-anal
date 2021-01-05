4d4b6eec07b39110d5d0f4ee10a92f2b1dc6636e
pragnasree14 <75874014+pragnasree14@users.noreply.github.com>
2021-01-05 20:30:34 +0530

    Update senti.py

--- senti.py
+++ senti.py
@@ -1,23 +1,14 @@
-import re
 import tweepy
-from tweepy import OAuthHandler
 from textblob import TextBlob
-consumer_key=''//you get from twitter 
-consumer_secret=''//you get from twitter
-access_token=''//you get from twitter
-access_token_secret=''//you get from twitter
-try:
-  #create OAuthHandler object
-  s.auth=OAuthHandler(consumer_key,consumer_secret)
-  #access to token and token secret
-  s.auth.set_access_token(access_token,access_token_secret)
-  #create tweepy 
-  s.api=tweepy.API(s.auth)
-
-  except:
-    print("Error:Authentication failed")
-tweets=api.searcg("IPL")
-for tweet in public_tweets;
-print tweet.text
-analysis=TextBlob(tweet.text)
-print(analysis.sentiment)
+consumer_key = ''#you get from twitter API
+consumer_secret = ''#you get from twitter API
+access_token = ''#you get from twitter API
+access_token_secret = ''#you get from twitter API
+auth=tweepy.OAuthHandler(consumer_key,consumer_secret)
+auth.set_access_token(access_token, access_token_secret)
+api=tweepy.API(auth)
+public_tweets = api.search('Mahesh Babu')
+for tweet in public_tweets:
+  print(tweet.text)
+  analysis=TextBlob(tweet.text)
+  print(analysis.sentiment)
